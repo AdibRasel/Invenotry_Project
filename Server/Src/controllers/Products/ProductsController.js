72181952c9551ac075b2455dddc03574f601304c
Rasal Hossain Adib <adibrasel.2022@gmail.com>
2023-06-17 10:00:46 +0600

    ProductList
    ProductList Product View API

--- Server/Src/controllers/Products/ProductsController.js
+++ Server/Src/controllers/Products/ProductsController.js
@@ -1,6 +1,7 @@
 const DataModel = require("../../models/Products/ProductModel");
 const CreateService = require("../../services/common/CreateService")
 const UpdateService = require("../../services/common/UpdateService");
+const ListTwoJoinService = require("../../services/common/ListTwoJoinService")
 
 // Create Product 
 exports.CreateProduct = async (req, res)=>{
@@ -11,5 +12,17 @@ exports.CreateProduct = async (req, res)=>{
 // Update Product 
 exports.UpdateProduct= async (req, res)=>{
     let Result = await UpdateService(req, DataModel)
+    res.status(200).json(Result)
+}
+
+//ProductList View
+exports.ProductList = async (req, res)=> {
+    let SearchRgx = {"$regex": req.params.searchKeyword, "$options": "i"}
+    let JoinStage1 = {$lookup: {from: "brands", localField: "BrandID", foreignField: "_id", as: "brands"}}
+    let JoinStage2 = {$lookup: {from: "categories", localField: "CategoryID", foreignField: "_id", as: "categories"}}
+    
+    let SearchArray = [{Name: SearchRgx}, {Unit: SearchRgx}, {Details: SearchRgx}, {'brands.Name': SearchRgx}, {'categories.Name': SearchRgx}]
+
+    let Result = await ListTwoJoinService(req, DataModel, SearchArray, JoinStage1, JoinStage2)
     res.status(200).json(Result)
 }
